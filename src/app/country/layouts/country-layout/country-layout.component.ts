5ee3f3222f822de3b9a2f4cc109e7971373eba83
Lancelot <lancelot@sunset.es>
2025-05-10 19:13:50 -0500

    .

---
+++ src/app/country/layouts/country-layout/country-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-layout',
+  imports: [],
+  templateUrl: './country-layout.component.html',
+  styleUrl: './country-layout.component.scss'
+})
+export class CountryLayoutComponent {
+
+}
